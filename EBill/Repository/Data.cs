e9c592b568a396ce6b9f4853dc9ad0e297f3d9b0
kunvar prabhat <kunvarprabhat44@gmail.com>
2023-03-02 14:38:52 +0530

    Add project files.

---
+++ EBill/Repository/Data.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EBill.Models;
+using EBill.Repository;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+
+namespace EBill.Repository
+{
+    public class Data : IData
+    {
+
+        public string ConntionString { get; set; }
+        public Data()
+        {
+            ConntionString = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+        }
+
+        public void SaveBillDetails(BillDetail deatails)
+        {
+            SqlConnection con = new SqlConnection(ConntionString);
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("spt_saveEBillDetails", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@CustomerName", deatails.CustomerName);
+                cmd.Parameters.AddWithValue("@MobileNumber", deatails.MobileNumber);
+                cmd.Parameters.AddWithValue("@Address", deatails.Address);
+                cmd.Parameters.AddWithValue("@TotalAmount", deatails.TotalAmount);
+
+                SqlParameter outputPara = new SqlParameter();
+                outputPara.DbType = DbType.Int32;
+                outputPara.Direction = ParameterDirection.Output;
+                outputPara.ParameterName = "@Id";
+                cmd.Parameters.Add(outputPara);
+                cmd.ExecuteNonQuery();
+                int id = int.Parse(outputPara.Value.ToString());
+                if (deatails.Items.Count > 0)
+                {
+                   SaveBillItems(deatails.Items,con,id); 
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        void SaveBillItems(List<Items> items, SqlConnection con, int id )
+        {
+            try
+            {
+                string qry = "insert into tbl_BillItems(ProductName,Price,Quantity) value";
+
+                foreach (Items item in items)
+                {
+                    qry += String.Format("('{0},{1},{2},{3}),",item.ProductName,item.Price,item.Quantity,id);
+
+                }
+                qry = qry.Remove(qry.Length-1);
+                SqlCommand cmd=new SqlCommand(qry, con);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        //////////////////
+
+      
+
+        void IData.SaveBillItems(List<Items> items, SqlConnection con, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+        
+}
---
+++ EBill/Repository/IData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EBill.Models;
+using System.Data.SqlClient;
+
+namespace EBill.Repository
+{
+    internal interface IData
+    {
+        void SaveBillDetails(BillDetail details);
+        void SaveBillItems(List<Items> items, SqlConnection con, int id);
+    }
+}
